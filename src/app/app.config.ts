975744c42e078ec9fc63aaeb5b69d472b985f5ba
Stefano Falda <stefano.falda@gmail.com>
2024-12-21 19:53:47 +0100

    Added PWA capabilities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,5 +9,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           })]
 };
